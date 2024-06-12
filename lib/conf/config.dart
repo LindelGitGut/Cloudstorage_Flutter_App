9995162796a6f1db22f5f198046f8539642283a1
LindelGitGut <alexandre.lindele@outlook.de>
2024-06-12 16:11:06 +0200

    changes to localip adress of dev pc

--- lib/conf/config.dart
+++ lib/conf/config.dart
@@ -1,4 +1,4 @@
 class Config {
-  static const String testURL ="http://127.0.0.1:8080";
+  static const String testURL ="http://192.168.188.51:8080";
   static const String prodUrl = 'https://lindele.de/Cloudstorage';
 }
