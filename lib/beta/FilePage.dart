c95cadcaa476b34eb5ecbc5ae4f909982bd3fe34
LindelGitGut <alexandre.lindele@outlook.de>
2024-06-13 12:51:23 +0200

    added some template

--- lib/beta/FilePage.dart
+++ lib/beta/FilePage.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
-class FilesPage extends StatelessWidget {
+class FilesPage extends StatefulWidget {
+  const FilesPage({super.key});
+
+  @override
+  State<FilesPage> createState() => _FilesPageState();
+}
+
+class _FilesPageState extends State<FilesPage> {
+  List<Widget> _getFileTiles() {
+    //TODO add Bloc Function to get all Current Files
+    return [
+      ListTile(
+        title: Text("Placeholder File 1"),
+      ),
+      ListTile(
+        title: Text("Placeholder File 2"),
+      ),
+      // Add more ListTiles for each file
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Files Page'),
+    return Column(
+      children: [
+        Center(
+          child: Text('Files Page'),
+        ),
+        IconButton(
+            onPressed: () {
+              print("Uploadbutton pressed!");
+            } /* _uploadNewFile,*/,
+            icon: Icon(Icons.upload)),
+        Text("Upload new File"),
+        SizedBox(height: 20), // Add spacing between elements
+        Text("Your saved Files"),
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: _getFileTiles(),
+          ),
+        )
+      ],
     );
   }
 }
